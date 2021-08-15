cf08f67dd447663b5027154941fb890daebbb857
AshFoster <ashfoster87@gmail.com>
2021-08-15 11:24:33 +0000

    Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+// Add event listeners to all buttons after the DOM has finshed loading
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "classic-version") {
+                alert("this will show classic");
+            } else if (this.getAttribute("data-type") === "lizard-spock") {
+                alert("this will show lizard spock");
+            } else if (this.getAttribute("data-type") === "rules") {
+                alert("this will show rules modal");
+            } else {
+                let playerSelection = this.getAttribute("data-type");
+                alert(`You clicked ${playerSelection}`);
+            }
+        });
+    }
+});
+
 function showClassic() {
 
 }
