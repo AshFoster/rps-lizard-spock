165187adc5d37332d2e05ad2dccf2d15f44bd789
AshFoster <ashfoster87@gmail.com>
2021-08-15 11:53:09 +0000

    Add code to show classic game when relevant button clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "classic-version") {
+                showClassic();
                 alert("this will show classic");
             } else if (this.getAttribute("data-type") === "lizard-spock") {
                 alert("this will show lizard spock");
@@ -19,7 +20,11 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function showClassic() {
-
+    document.getElementById("lizard-button").style.display = "none";
+    document.getElementById("spock-button").style.display = "none";
+    document.getElementById("lizard-rules").style.display = "none";
+    document.getElementsByClassName("main-menu-area")[0].style.display = "none";
+    document.getElementsByClassName("main-area")[0].style.display = "flex";
 }
 
 function showLizardSpock() {
