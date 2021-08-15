55443c2ebeac2c741c765f38ea3cc5fc45e49e9a
AshFoster <ashfoster87@gmail.com>
2021-08-15 11:10:30 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+function showClassic() {
+
+}
+
+function showLizardSpock() {
+
+}
+
+function runGame() {
+
+}
+
+function playClassic() {
+
+}
+
+function playLizardSpock() {
+
+}
+
+function showRules() {
+
+}
+
+function incrementPlayerScore() {
+
+}
+
+function incrementComputerScore() {
+
+}
+
+function checkWinner() {
+
+}
+
+function endGame() {
+
+}
